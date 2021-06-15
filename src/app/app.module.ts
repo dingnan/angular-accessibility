c634ed494ae5f5427433ef6455f8770b37c96fc7
dingnan <nandingb@gmail.com>
2021-06-15 15:14:05 -0400

    first commit - fruit list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent, ListComponent, ListItemComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ListComponent, ListItemComponent
   ],
   imports: [
     BrowserModule
